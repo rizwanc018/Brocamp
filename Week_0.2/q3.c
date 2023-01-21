0e2cb57f09a2092715ca0edfd7676dfda2d730d7
rizwanc018 <22193871+rizwanc018@users.noreply.github.com>
2023-01-21 14:57:09 +0530

    Changes to be committed: 	new file:   Week_0.2/q3 	modified:   Week_0.2/q3.c

--- Week_0.2/q3.c
+++ Week_0.2/q3.c
@@ -1,1 +1,21 @@
 // Write a program to find Second Largest Number in an Array
+
+#include <stdio.h>
+int main() {
+    int arr[] = {80, 22,47,56,74,89,45,10};
+    int l = sizeof(arr)/sizeof(arr[0]);
+
+    int max = arr[0];
+    int smax = -2;
+
+    for(int i = 0; i < l; i++) {
+        if(arr[i] > max) {
+            smax = max;
+            max = arr[i];
+        }else if(arr[i] > smax) {
+            smax = arr[i];
+        } 
+    }
+
+    printf("Second largest = %d\n", smax);
+}
